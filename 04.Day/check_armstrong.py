9e5ee1afe2b6c73b8295e786f6f78851e57f0dd7
Aparna Raha <aparnaraha2912@gmail.com>
2024-04-20 21:35:55 +0530

    Update day 4

--- 04.Day/check_armstrong.py
+++ 04.Day/check_armstrong.py
@@ -1,9 +1,12 @@
-# Program to check if the number is Armstrong or not 
+# Program to check if the number is Armstrong or not
+
+# take input from user
 num = int(input("enter a number here: "))
 
 sum = 0
 temp = num
 
+# checking using while loop
 while temp > 0:
   digit = temp % 10
   cube = digit ** 3
