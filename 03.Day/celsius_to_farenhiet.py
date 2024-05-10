51b16068a7ef32e2049ab03ce16a687320c07b83
Aparnaraha <aparnaraha2912@gmail.com>
2024-05-10 10:50:33 +0530

    Changining to SQL file

--- 03.Day/celsius_to_farenhiet.py
+++
@@ -1,7 +0,0 @@
-
-
-celsius  = int(input("enter temperature in celsius: "))
-
-farenhiet = (celsius*(9/5)+32)
-
-print("converted value is ", farenhiet)
--- 03.Day/generate_random_number.py
+++
@@ -1,5 +0,0 @@
-import random
-
-num = random.randint(1,9)
-
-print(num)
